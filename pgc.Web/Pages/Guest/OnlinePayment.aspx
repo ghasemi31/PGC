30738de20baea8126243f6d4a7473867b844e6bf
MostafaTaghipour <mostafa.taghipour@ymail.com>
2016-08-11 18:58:45 +0430

    sgffhffdgh

--- pgc.Web/Pages/Guest/OnlinePayment.aspx
+++ pgc.Web/Pages/Guest/OnlinePayment.aspx
@@ -5,12 +5,9 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cphContentPage" Runat="Server">
     <%--https://sep.shaparak.ir/Payment.aspx--%>
-    <form action="<%=new pgc.Business.Core.SamanOnlinePayment().SamanUrl %>" method="post" name="onlinePaymentForm">
-        <input type="hidden" id="Amount" name="Amount" style="visibility: hidden" value="<%=online.Amount.ToString() %>" />
-        <input type="hidden" id="ResNum" name="ResNum" style="visibility: hidden" value="<%=online.ResNum %>" />
-        <input type="hidden" id="Wage" name="Wage" style="visibility: hidden" value="0" />
-        <input type="hidden" id="MID" name="MID" style="visibility: hidden" value="<%=new pgc.Business.Core.SamanOnlinePayment().MerchantID%>" />
-        <input type="hidden" id="RedirectURL" name="RedirectURL" style="visibility: hidden" value="<%=new pgc.Business.Core.SamanOnlinePayment().RedirectURL %>" />
+     <form action="<%=kFrameWork.Business.OptionBusiness.GetText(pgc.Model.Enums.OptionKey.Mellat_BankURL) %>" method="post" name="payment">
+         <input type="hidden" id="RefId" name="RefId" value="<%=RefId%>"/>
+       
 
         <div class="container" style="text-align: center;margin-top: 2em;font-size: 1.2em;">
             کاربر گرامی، برای اتصال به درگاه بانک سامان لطفا منتظر بمانید.
--- pgc.Web/Pages/Guest/OnlinePayment.aspx.cs
+++ pgc.Web/Pages/Guest/OnlinePayment.aspx.cs
@@ -1,17 +1,29 @@
-﻿using kFrameWork.UI;
+﻿using kFrameWork.Business;
+using kFrameWork.Model;
+using kFrameWork.UI;
+using kFrameWork.Util;
 using pgc.Business;
+using pgc.Business.Payment.OnlinePay;
 using pgc.Model;
 using pgc.Model.Enums;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Pages_Guest_OnlinePayment : BasePage
 {
-    public OnlinePayment online = new OnlinePayment();
+
+    public GameOrder order = null;
+    PaymentBusiness pay_business = new PaymentBusiness();
+    public string RefId;
+    long PayID;
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -19,11 +31,81 @@ public partial class Pages_Guest_OnlinePayment : BasePage
             Response.Redirect(GetRouteUrl("guest-default", null));
         else
         {
-            online = new OnlinePaymentBusiness().Retrieve(this.GetQueryStringValue<long>(QueryStringKeys.id));
+            order = pay_business.RetrieveOrder(this.GetQueryStringValue<long>(QueryStringKeys.id));
 
-            if (online.Order.IsPaid || online.ResultTransaction != 0)
+            if (order.IsPaid)
                 Response.Redirect(GetRouteUrl("guest-default", null));
 
+            OperationResult res = new OperationResult();
+            res = new pgc.Business.Payment.OnlinePay.PaymentBusiness().CreatePayment(order.ID, OnlineGetway.MellatBankGateWay);
+
+            if (res.Result == ActionResult.Done)
+            {
+                PayID = (long)res.Data["ResNum"];
+                Post();
+            }
         }
+    }
+
+
+    private void Post()
+    {
+
+        try
+        {
+            string result;
+            string redirectUrl = ConfigurationManager.AppSettings["WebSiteURL"] + "/Pages/Guest/CallBackUrl.aspx";
+            long terminalId = ConvertorUtil.ToInt64(OptionBusiness.GetText(pgc.Model.Enums.OptionKey.Mellat_TerminalId));
+            BypassCertificateError();
+
+
+            pgc.Business.ir.shaparak.bpm.PaymentGatewayImplService bpService = new pgc.Business.ir.shaparak.bpm.PaymentGatewayImplService();
+            result = bpService.bpPayRequest(terminalId,
+                OptionBusiness.GetText(pgc.Model.Enums.OptionKey.Mellat_UserName),
+                OptionBusiness.GetText(pgc.Model.Enums.OptionKey.Mellat_Password),
+                PayID,
+                (order.PayableAmount),
+                DateTime.Now.ToString("yyyyMMdd"),
+                DateTime.Now.ToString("HHmmss"),
+                "",
+                redirectUrl,
+               0);
+
+            String[] resultArray = result.Split(',');
+
+            int State = ConvertorUtil.ToInt32(resultArray[0]);
+
+            if (State == (int)MellatOnlinePaymentState.OK)
+                RefId = resultArray[1];
+            else
+            {
+                string message = EnumUtil.GetEnumElementPersianTitle((MellatOnlinePaymentState)State);
+                UserSession.AddCompeleteMessage(UserMessage.CreateUserMessage(0, "msg", 4, 2, 1, message));
+
+                pay_business.ChangeOrderState(PayID, State);
+
+
+                Response.Redirect(GetRouteUrl("user-gamedetail", new { id = order.ID }));
+
+            }
+        }
+        catch (Exception exp)
+        {
+            UserSession.AddMessage(UserMessageKey.BankWebServiceNoResponse);
+
+        }
+    }
+
+    void BypassCertificateError()
+    {
+        ServicePointManager.ServerCertificateValidationCallback +=
+            delegate(
+                Object sender1,
+                X509Certificate certificate,
+                X509Chain chain,
+                SslPolicyErrors sslPolicyErrors)
+            {
+                return true;
+            };
     }
 }
